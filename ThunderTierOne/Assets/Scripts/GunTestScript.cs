c974bbfb222a72355f47cb6df77618f279721185
CoJangKeun <bbh1039@naver.com>
2021-03-04 11:03:34 +0900

    == Gun Laser Temp Added ==

---
+++ ThunderTierOne/Assets/Scripts/GunTestScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GunTestScript : MonoBehaviour
+{
+    public Transform MuzzleTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawLine(MuzzleTransform.position, MuzzleTransform.forward * 10.0f, Color.green);
+    }
+}
